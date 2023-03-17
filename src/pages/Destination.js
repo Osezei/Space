98e7f3e0e8cf50b550bab10596ef0b2fe1dde1c4
osezei <john.osezei@yahoo.com>
2023-03-17 20:35:49 +0100

    mobile

--- src/pages/Destination.js
+++ src/pages/Destination.js
@@ -8,88 +8,92 @@ const Destination = () => {
   const [toggled, setToggled] = useState(1);
 
   return (
-    <Section className="page destination text-white">
-      <Navigation />
-      <div className="lg:grid lg:grid-cols-2">
+    <Section className="page destination text-white ">
+      <div className="container relative">
+        <Navigation />
+        <div className="block lg:grid lg:grid-cols-2">
+          <div className="mt-5 lg:mt-10 flex justify-center">
+            <h3 className="text-sm lg:text-2xl  text-home uppercase lg:w-full">
+              <span className="text-home text-[#d0d6f976] pr-2 ">01</span> Pick
+              your destination
+            </h3>
+          </div>
+
+          <div className="flex justify-center mt-[300px] lg:justify-start  text-sm lg:mt-24">
+            {destinationInfo.map(({ id, title }) => {
+              return (
+                <button
+                  key={id}
+                  onClick={() => setToggled(id)}
+                  className={`${
+                    toggled === id
+                      ? "mr-6 text-home uppercase tracking-wider title-fixed"
+                      : "mr-6 text-home uppercase tracking-wider title-destination"
+                  }`}
+                >
+                  {title}
+                </button>
+              );
+            })}
+          </div>
+        </div>
+
         <div>
-          <h3 className="text-sm mt-10 lg:text-2xl container text-home uppercase mx-auto lg:w-full">
-            <span className="text-home text-[#d0d6f976] pr-2">01</span> Pick
-            your destination
-          </h3>
-        </div>
-
-        <div className="flex justify-center lg:justify-start  text-sm mt-24">
-          {destinationInfo.map(({ id, title }) => {
-            return (
-              <button
-                key={id}
-                onClick={() => setToggled(id)}
-                className={`${
-                  toggled === id
-                    ? "mr-6 text-home uppercase tracking-wider title-fixed"
-                    : "mr-6 text-home uppercase tracking-wider title-destination"
-                }`}
-              >
-                {title}
-              </button>
-            );
-          })}
-        </div>
-      </div>
-
-      <div>
-        {destinationInfo.map(
-          ({
-            title,
-            note,
-            image,
-            id,
-            distance,
-            kilometers,
-            km,
-            time,
-            days,
-          }) => {
-            return (
-              <article key={id} className="">
-                {toggled === id ? (
-                  <div className="block lg:grid grid-cols-2 small-container lg:container">
-                    <div className=" w-[250px] lg:w-[450px] lg:mt-5 lg:ml-6">
-                      <img src={image} alt={title} className="" />
-                    </div>
-                    <div>
-                      <h6 className="text-6xl lg:text-9xl uppercase py-8 text-home">
-                        {title}
-                      </h6>
-                      <p className="top-wording text-sm leading-loose">
-                        {note}
-                      </p>
-                      <div className="mt-14 mb-10 h-[0.05rem] w-[36rem] bg-[#d0d6f976] "></div>
-                      <div className="grid grid-rows-2 lg:grid-cols-2">
-                        <div>
-                          <p className="uppercase text-home text-sm">
-                            {" "}
-                            {distance}
-                          </p>
-                          <h3 className="text-3xl text-home ">
-                            {kilometers}
-                            <span className="uppercase"> {km}</span>
-                          </h3>
-                        </div>
-                        <div className="mt-3">
-                          <p className="uppercase text-home text-sm">{time}</p>
-                          <h3 className="text-3xl text-home uppercase">
-                            {days}
-                          </h3>
+          {destinationInfo.map(
+            ({
+              title,
+              note,
+              image,
+              id,
+              distance,
+              kilometers,
+              km,
+              time,
+              days,
+            }) => {
+              return (
+                <article key={id} className="">
+                  {toggled === id ? (
+                    <div className=" mt-[10px] grid-rows-2 place-items-center lg:grid grid-cols-2 ">
+                      <div className=" w-[220px] lg:w-[450px] lg:mt-5 lg:ml-6 absolute top-[150px] left-[80px]">
+                        <img src={image} alt={title} className="" />
+                      </div>
+                      <div className="">
+                        <h6 className="text-6xl lg:text-9xl uppercase py-5 lg:py-8 text-home text-center ">
+                          {title}
+                        </h6>
+                        <p className="top-wording text-sm leading-loose text-center">
+                          {note}
+                        </p>
+                        <div className="my-8 lg:mt-14 lg:mb-10 w-[350px] h-[0.05rem] lg:w-[36rem] bg-[#d0d6f976] mx-auto"></div>
+                        <div className="grid grid-rows-2 place-items-center lg:grid-cols-2">
+                          <div>
+                            <p className="uppercase text-home text-sm">
+                              {" "}
+                              {distance}
+                            </p>
+                            <h3 className="text-3xl text-home ">
+                              {kilometers}
+                              <span className="uppercase"> {km}</span>
+                            </h3>
+                          </div>
+                          <div className="mt-3">
+                            <p className="uppercase text-home text-sm">
+                              {time}
+                            </p>
+                            <h3 className="text-3xl text-home uppercase">
+                              {days}
+                            </h3>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                ) : null}
-              </article>
-            );
-          }
-        )}
+                  ) : null}
+                </article>
+              );
+            }
+          )}
+        </div>
       </div>
     </Section>
   );
